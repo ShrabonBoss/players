ed13673919600c52ad672d5068fd132305e95a0b
ShrabonBoss <shrabonboss6519@gmail.com>
2025-03-29 14:13:58 +0600

    erroes handeling and updating

--- src/main/java/com/pl/players/repository/PlayerRepository.java
+++ src/main/java/com/pl/players/repository/PlayerRepository.java
@@ -1,6 +1,6 @@
 package com.pl.players.repository;
 
-import model.Player;
+import com.pl.players.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
